c18c798febd07e2f33ceb4df34e9083bda3d207d
Michael Tripp <85533231+Mctripp10@users.noreply.github.com>
2023-10-31 07:22:42 -0400

    Icon links + content fill
    Added links for social media icons and started filling in some more of my own content. Also added some different purple/pink coloring to the 3 tiles

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -62,16 +62,16 @@ export default function Home() {
                 Nice to meet you.
               </h3>
               <div className="text-5xl flex justify-center gap-16 py-10 text-gray-600 dark:text-gray-400">
-                <BsLinkedin />
-                <BsGithub />
-                <BsInstagram />
+                <a href="https://www.linkedin.com/in/michaelctripp/" target="_blank"><BsLinkedin /></a>
+                <a href="https://github.com/Mctripp10" target="_blank"><BsGithub /></a>
+                <a href="https://www.instagram.com/mc_tripp/" target="_blank"><BsInstagram /></a>
               </div>
             </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <h3 className="text-3xl py-1 dark:text-white ">My Background</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
               developer, I've done remote work for
@@ -86,54 +86,54 @@ export default function Home() {
             </p>
           </div>
           <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-gradient-to-b dark:from-pink-500 dark:to-violet-600 flex-1">
               <Image className="mx-auto" src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 text-gray-800 ">
+              <h3 className="text-lg font-medium pt-8 pb-2 text-gray-800 dark:text-white">
                 Beautiful Designs
               </h3>
-              <p className="py-2 text-gray-800">
+              <p className="py-2 text-gray-800 dark:text-white">
                 Creating elegant designs suited for your needs following core
                 design theory.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h4 className="py-4 text-teal-300">Design Tools I Use</h4>
+              <p className="text-gray-800 dark:text-white py-1">Photoshop</p>
+              <p className="text-gray-800 dark:text-white py-1">Illustrator</p>
+              <p className="text-gray-800 dark:text-white py-1">Figma</p>
+              <p className="text-gray-800 dark:text-white py-1">Indesign</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gradient-to-b dark:from-pink-500 dark:to-violet-600 flex-1">
               <Image className="mx-auto" src={code} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 text-gray-800">
+              <h3 className="text-lg font-medium pt-8 pb-2 text-gray-800 dark:text-white">
                 Code your dream project
               </h3>
-              <p className="py-2 text-gray-800">
+              <p className="py-2 text-gray-800 dark:text-white">
                 Do you have an idea for your next great website? Let's make it a
                 reality.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h4 className="py-4 text-teal-300">Design Tools I Use</h4>
+              <p className="text-gray-800 dark:text-white py-1">Photoshop</p>
+              <p className="text-gray-800 dark:text-white py-1">Illustrator</p>
+              <p className="text-gray-800 dark:text-white py-1">Figma</p>
+              <p className="text-gray-800 dark:text-white py-1">Indesign</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gradient-to-b dark:from-pink-500 dark:to-violet-600 flex-1">
               <Image className="mx-auto" src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 text-gray-800">Consulting</h3>
-              <p className="py-2 text-gray-800">
+              <h3 className="text-lg font-medium pt-8 pb-2 text-gray-800 dark:text-white">Consulting</h3>
+              <p className="py-2 text-gray-800 dark:text-white">
                 Are you interested in feedback for your current project? I can
                 give you tips and tricks to level it up.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h4 className="py-4 text-teal-300">Design Tools I Use</h4>
+              <p className="text-gray-800 dark:text-white py-1">Illustrator</p>
+              <p className="text-gray-800 dark:text-white py-1">Figma</p>
+              <p className="text-gray-800 dark:text-white py-1">Indesign</p>
+              <p className="text-gray-800 dark:text-white py-1">Photoshop</p>
             </div>
           </div>
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <h3 className="text-3xl py-1 dark:text-white ">Projects</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
               developer, I've done remote work for
