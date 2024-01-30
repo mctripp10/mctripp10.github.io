98c1c8dcc26392f4be7280b0468e6eac43268075
Michael Tripp <85533231+Mctripp10@users.noreply.github.com>
2024-01-30 07:32:05 -0500

    Project card src url fix
    Figured out the issue: the names were inconsistent between both files. The parameter in ProjectCard.jsx was called srcUrl while the argument in ProjectSection.jsx was called srcLink.....

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -34,11 +34,11 @@ const ProjectsSection = () => {
             <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
                 {projectsData.map((project) => (
                     <ProjectCard 
-                        srcLink = {project.src}
                         key={project.id} 
                         title={project.title} 
                         description={project.description} 
                         imgUrl={project.image} 
+                        srcUrl = {project.src}
                     />
                 ))}
             </div>
