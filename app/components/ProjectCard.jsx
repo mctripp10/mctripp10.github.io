7151c9824d992b802e84e1214c916ff9aebeae38
Michael Tripp <85533231+Mctripp10@users.noreply.github.com>
2024-02-04 16:56:05 -0500

    Fixed upper project card border
    The upper border was set to xl and the bottom was set to 3xl, so I changed them both to 3xl for consistency.

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -9,10 +9,10 @@ const ProjectCard = ({ title, description, imgUrl, srcUrl, tags }) => {
         shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]
         ">
             <div
-                className="h-52 md:h-72 rounded-t-xl"
+                className="h-52 md:h-72 rounded-t-3xl"
                 style={{ background: `url(${imgUrl})`, backgroundSize: "cover", backgroundPosition: "center" }}
             ></div>
-            <div className='flex h-28 mx-5 text-centertext-white rounded-b-xl'>
+            <div className='flex h-28 mx-5 text-centertext-white'>
                 <div className='m-auto'>
                     <h5 className='text-xl font-semibold'>{title}</h5>
                     <p>{description}</p>
