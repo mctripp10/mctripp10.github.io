f3688a3f5962650935f49501c7e468e04b8a945d
Michael Tripp <85533231+Mctripp10@users.noreply.github.com>
2024-02-07 07:40:54 -0500

    Started contact page
    Contact page is started and almost finished. Not sure about the spacing yet but really just need to fix some styling to match my website (change teal to pink/purple look).
    
    Additionally, I want to make the form useable so that it will actually send me a message when someone fills out the form.

--- app/pages/home/page.js
+++ app/pages/home/page.js
@@ -2,23 +2,8 @@
 
 import Head from "next/head";
 import { useState } from "react";
-import {
-  AiFillTwitterCircle,
-  AiFillLinkedin,
-  AiFillYoutube,
-} from "react-icons/ai";
-import { BsFillMoonStarsFill, BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
-{/* import code from "../public/code.png";
-import design from "../public/design.png";
-import consulting from "../public/consulting.png";
-import Image from "next/image";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
-*/}
+import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
+
 import NavBarHome from "../../components/NavBarHome";
 
 export default function Home() {
