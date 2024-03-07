8087a415d3e5326263c185fac8eb1f8aecaa76bd
Michael Tripp <85533231+Mctripp10@users.noreply.github.com>
2024-03-06 19:25:26 -0500

    Update about text
    Changed some wording and fixed a typo

--- app/about/page.js
+++ app/about/page.js
@@ -32,11 +32,11 @@ export default function About() {
 
                   As a {' '}
                   <span className="font-bold bg-gradient-to-br from-pink-500 to-violet-600 text-transparent bg-clip-text"> 
-                  mathematician who loves to write code
+                  mathematician with a keen interest in coding
                   </span>
-                  , I find the ability use computer programming to implement solutions to mathematical problems very fun. 
+                  , I find the ability to use computer programming to implement solutions to mathematical problems very fun. 
                   Furthermore, creation and development have always been a passion of mine, so I love exploring what is possible to build with the technologies at my disposal.
-                  The process of building something from nothing and then finding ways to use that creation to solve real-world problems is particularly gratifying to me. <br /><br />
+                  The process of creating something from nothing and then finding ways to use that creation to solve real-world problems is particularly gratifying to me. <br /><br />
 
                   Prior to my work as a data analyst, I worked as a desktop support student tech for Westminster College&apos;s ITS department for almost the entirety of my college career.
                   I graduated from Westminster College in May 2023 with a {' '}
