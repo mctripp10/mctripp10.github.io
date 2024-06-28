e09bb6ac494b4bcd9c77120e65aaba6357a1187e
Michael Tripp <85533231+Mctripp10@users.noreply.github.com>
2024-06-28 13:08:26 -0400

    Added .env to fix image not showing up

--- components/ProjectsSection.jsx
+++ components/ProjectsSection.jsx
@@ -14,7 +14,7 @@ const projectsData = [
         id: 2,
         title: 'Clustering Iris Flower Data',
         description: 'Utilizes unsupervised learning clustering algorithms to analyze and build predictive models for the iris flower dataset.',
-        image: '/images/projects/clustering-iris-flower-data-proj.png',
+        image: './images/projects/clustering-iris-flower-data-proj.png',
         src: 'https://github.com/Mctripp10/clustering-iris-flower-data',
         tags: ['All', 'Python', 'TensorFlow', 'Keras']
     },
@@ -22,7 +22,7 @@ const projectsData = [
         id: 3,
         title: 'Wi-Fi Positioning System',
         description: 'Application to approximate user device location by scanning nearby Wi-Fi hotspots.',
-        image: '/images/projects/wifi-positioning-app-proj-inverted.png',
+        image: './images/projects/wifi-positioning-app-proj-inverted.png',
         src: 'https://github.com/Mctripp10/wi-fi-positioning-app',
         tags: ['All', 'Python', 'Windows WLAN API']
     },
@@ -30,7 +30,7 @@ const projectsData = [
         id: 4,
         title: 'Bouncy Bouncy',
         description: 'GUI application that displays a smaller object bouncing around an outer boundary, where both object and boundary are drawn by the user.',
-        image: '/images/projects/bouncy-bouncy-proj.png',
+        image: './images/projects/bouncy-bouncy-proj.png',
         src: 'https://github.com/Mctripp10/bouncy-bouncy',
         tags: ['All', 'C++', 'OpenGL']
     },
@@ -38,23 +38,23 @@ const projectsData = [
         id: 5,
         title: 'Memory Game',
         description: 'Memory game in which the player must attempt to remove all cards by flipping over matching pairs.',
-        image: '/images/projects/memory-game-proj.png',
+        image: './images/projects/memory-game-proj.png',
         src: 'https://github.com/Mctripp10/memory-game',
         tags: ['All', 'C++', 'OpenGL']
     },
     {
-        id: 3,
+        id: 6,
         title: 'Melon Mall Website',
         description: 'A "for fun" website dedicated to the celebration of melons and all they have to offer.',
-        image: '/images/projects/melon-mall-proj.png',
+        image: './images/projects/melon-mall-proj.png',
         src: 'https://github.com/Mctripp10/melon-mall',
         tags: ['All', 'HTML', 'CSS', 'JavaScript']
     },
     {
-        id: 4,
+        id: 7,
         title: 'This Website',
         description: 'My personal portfolio website made using Next.js and Tailwind CSS.',
-        image: '/images/projects/react-portfolio-proj.png',
+        image: './images/projects/react-portfolio-proj.png',
         src: 'https://github.com/Mctripp10/react-portfolio',
         tags: ['All', 'Next.js', 'Tailwind CSS']
     }
